e2f6ef6fef5aa7de4ea55a7a135358fb19d30e80
edcity-felixwong <hkecgl-felixwong@hkecl.net>
2023-10-20 09:36:00 +0800

    style(sb-font-code)

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,40 +1,39 @@
-import { create } from "@storybook/theming/create";
+import { create } from "@storybook/theming/create"
 
 export default create({
-    base: 'light',
+  base: "light",
   // Typography
   fontBase: `'Inter',"Open Sans", Roboto, 'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif`,
-  fontCode: ` Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace`,
+  fontCode: `'Source Code Pro', Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace`,
 
-  brandTitle: 'My custom Storybook',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
-  brandTarget: '_self',
+  brandTitle: "My custom Storybook",
+  brandUrl: "https://example.com",
+  brandImage: "https://storybook.js.org/images/placeholders/350x150.png",
+  brandTarget: "_self",
 
   //
-  colorPrimary: '#3A10E5',
-  colorSecondary: '#585C6D',
+  colorPrimary: "#3A10E5",
+  colorSecondary: "#585C6D",
 
   // UI
-  appBg: '#ffffff',
-  appContentBg: '#ffffff',
-//   appBorderColor: '#585C6D',
+  appBg: "#ffffff",
+  appContentBg: "#ffffff",
+  //   appBorderColor: '#585C6D',
   appBorderRadius: 16,
 
   // Text colors
-  textColor: '#10162F',
-  textInverseColor: '#ffffff',
+  textColor: "#10162F",
+  textInverseColor: "#ffffff",
 
   // Toolbar default and active colors
-//   barTextColor: '#9E9E9E',
-//   barSelectedColor: '#585C6D',
-//   barBg: '#ffffff',
+  //   barTextColor: '#9E9E9E',
+  //   barSelectedColor: '#585C6D',
+  //   barBg: '#ffffff',
 
   // Form colors
-//   inputBg: '#ffffff',
-//   inputBorder: '#10162F',
-//   inputTextColor: '#10162F',
+  //   inputBg: '#ffffff',
+  //   inputBorder: '#10162F',
+  //   inputTextColor: '#10162F',
   inputBorderRadius: 16,
-//   base: "light",
-
-});
+  //   base: "light",
+})
